3e30fc4b9499f80fc4949dba39e15921b9cf9f6d
ahmetkca <ahmet.karapinar@ontariotechu.net>
2022-08-12 13:28:44 -0400

    more code splitting use cases

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,20 +1,25 @@
-import React, { lazy, Suspense } from "react";
-// import One from "../components/One";
-// import Two from "../components/Two";
-// import Three from "../components/Three";
+import React, { useState, lazy, Suspense } from "react";
+
 const One = lazy(() => import("../components/One"));
 const Two = lazy(() => import("../components/Two"));
 const Three = lazy(() => import("../components/Three"));
 
 const About = () => {
+    const [toggleComponents, setToggleComponents] = useState(false);
+
     return (
         <div style={{ backgroundColor: 'green', color: 'white' }}>
         <h1>About</h1>
-        <Suspense fallback={<div>Components are loading...</div>}>
-            <One />
-            <Two />
-            <Three />
+        {toggleComponents && (
+                <Suspense fallback={<div>Components are loading...</div>}>
+                <One />
+                <Two />
+                <Three />
         </Suspense>
+        )}
+        <button onClick={() => setToggleComponents(!toggleComponents)}>
+            {toggleComponents ? "Hide Components" : "Show Components"}
+        </button>
         </div>
     );
 }
