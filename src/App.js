17a68a874a763713adbab558ed9f6982d7fec580
ahmetkca <ahmet.karapinar@ontariotechu.net>
2022-08-12 13:22:46 -0400

    Route based code splitting

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import './App.css';
+import { lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Articles from './pages/Articles';
+
 import { Layout } from './components/Layout';
+const Home = lazy(() => import('./pages/Home'));
+const About = lazy(() => import('./pages/About'));
+const Articles = lazy(() => import('./pages/Articles'));
+
+const HOME_ROUTE = () => (
+  <Suspense fallback={<div>Loading home page...</div>}>
+    <Home />
+  </Suspense>
+)
+
+const ABOUT_ROUTE = () => (
+  <Suspense fallback={<div>Loading about page...</div>}>
+    <About />
+  </Suspense>
+)
+
+const ARTICLES_ROUTE = () => (
+  <Suspense fallback={<div>Loading articles page...</div>}>
+    <Articles />
+  </Suspense>
+)
 
 function App() {
   return (
     <Routes >
       <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="articles" element={<Articles />} />
+        <Route index element={<HOME_ROUTE />} />
+        <Route path="about" element={<ABOUT_ROUTE />} />
+        <Route path="articles" element={<ARTICLES_ROUTE />} />
       </Route>
     </Routes>
   );
